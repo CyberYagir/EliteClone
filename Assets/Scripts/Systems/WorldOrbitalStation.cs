b84c5acc8b5fde66d7d5cdb26a7fb71a1f9fbb22
Yagir <76903670+CyberYagir@users.noreply.github.com>
2021-12-05 13:21:35 +0300

    StationSeed

--- Assets/Scripts/Systems/WorldOrbitalStation.cs
+++ Assets/Scripts/Systems/WorldOrbitalStation.cs
@@ -1,8 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 public class WorldOrbitalStation : MonoBehaviour
 {
+    [SerializeField] private int uniqSeed;
     public Transform spawnPoint;
+    private void Start()
+    {
+        uniqSeed = 0;
+        foreach (var ch in Encoding.ASCII.GetBytes(transform.name))
+        {
+            uniqSeed += ch;
+        }
+        uniqSeed *= uniqSeed;
+    }
 }
