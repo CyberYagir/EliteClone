c154c5d2f66c232f82e26c0333757b0cff742aea
Yagir <76903670+CyberYagir@users.noreply.github.com>
2022-06-25 21:11:48 +0300

    Fixes

---
+++ Assets/PlayerTDSCamera.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Core.Demo;
+using UnityEngine;
+
+namespace Core.CommunistsBase
+{
+    public class PlayerTDSCamera : DemoPlayerCamera
+    {
+
+        public static PlayerTDSCamera Instance;
+        public enum CameraModes
+        {
+            Control,
+            OutsideControl
+        }
+
+        public CameraModes mode;
+        private Quaternion startRotation;
+
+        public void Start()
+        {
+            startRotation = transform.rotation;
+        }
+
+        public void FixedUpdate()
+        {
+            if (mode == CameraModes.Control)
+            {
+                transform.rotation = Quaternion.LerpUnclamped(transform.rotation, startRotation, 10 * Time.fixedDeltaTime);
+                UpdateCamera(Time.fixedDeltaTime);
+            }
+        }
+
+        public static void ChangeMode(CameraModes mode)
+        {
+            if (Instance == null) return;
+            Instance.mode = mode;
+        }
+    }
+}
--- Assets/Scripts/Core/Demo/DemoPlayerCamera.cs
+++ Assets/Scripts/Core/Demo/DemoPlayerCamera.cs
@@ -1,14 +1,27 @@
+using System;
 using UnityEngine;
 
 namespace Core.Demo
 {
     public class DemoPlayerCamera : MonoBehaviour
     {
-        [SerializeField] private Vector3 offcet;
-        [SerializeField] private Transform player;
+        [SerializeField] protected Vector3 offcet;
+        [SerializeField] protected Transform player;
+        
+
+        private void Start()
+        {
+            
+        }
+
         private void FixedUpdate()
         {
-            transform.position = Vector3.Lerp(transform.position, player.transform.position + offcet, 10 * Time.fixedDeltaTime);
+        }
+
+
+        public void UpdateCamera(float delta)
+        {
+            transform.position = Vector3.Lerp(transform.position, player.transform.position + offcet, 10 * delta);
         }
     }
 }
